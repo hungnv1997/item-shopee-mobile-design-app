05057269b9b2b1d7fd034a723a84591d9b1ab2f3
Nguyen Viet Hung <hung.nv155790.hust@gmail.com>
2021-08-13 21:30:54 +0700

    Adđ img file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="item-page">
+        <img src="#" alt="Nút bấm chơi game pubg" />
+        <Nav/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div>
+            <div className="item-page__nav">
+                <img src="../asset/icon/footer/home.svg" style={{width:"20px"}}/>
+                <img src="../asset/icon/footer/shopping-cart.svg"/>
+                <img src="../asset/icon/footer/heart.svg"/>
+                <img src="../asset/icon/footer/user.svg"/>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
